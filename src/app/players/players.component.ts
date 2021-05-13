9e6120e8e20002addf96aa9e167d080eeb4f8515
BouttaBeBanned <Harvard214>
2021-05-13 01:42:21 -0400

    Reverted players.components.ts

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from '../player';
 import { PLAYERS } from '../TheBoys';
-import { LolApi, Constants } from 'twisted';
-const api = new LolApi('RGAPI-0a9a87f9-06c8-43bd-80f3-b6fdebb35fdb');
+
 
 @Component({
   selector: 'app-players',
@@ -12,24 +11,13 @@ const api = new LolApi('RGAPI-0a9a87f9-06c8-43bd-80f3-b6fdebb35fdb');
 export class PlayersComponent implements OnInit {
   players = PLAYERS;
   selectedPlayer?: Player;
-  onSelect(player: Player): void {
-    this.selectedPlayer = player;
-    var summoner = "";
-    api.Summoner.getByName(player.sumName, Constants.Regions.AMERICA_NORTH).then(res =>
-      {
-        summoner = res.response.id;
-      })
-    api.Champion.masteryBySummoner(summoner, Constants.Regions.AMERICA_NORTH).then(res =>
-      {
-        api.DataDragon.getChampion(res.response[1].championId).then(res =>{
-          if(this.selectedPlayer){
-            this.selectedPlayer.highMastChamp = res.name;
-          }    
-        })
-      })
-  } 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSelect(player: Player): void{
+    this.selectedPlayer = player;
+  }
+
 }
